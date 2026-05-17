a1bad61c0341ffbc5269b398648e7ed114957a68
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-17 23:59:06 +0200

    feat: add `main.cpp` with simple example
    Adds main executable file to the project with a simple initialization of `File` class on stack and a few getters to get stored values wrapped inside a try/catch block.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "file.hpp"
+
+int main() {
+  try {
+    const dart::File db = dart::File::Create<100'000>("dart.db");
+
+    std::cout << "path: " << db.path() << '\n';
+    std::cout << "size: " << db.size() << '\n';
+    std::cout << "page_mode: " << std::to_underlying(db.page_mode()) << '\n';
+    std::cout << "pages in total: " << db.size() / std::to_underlying(db.page_mode()) << '\n';
+  } catch (const std::bad_expected_access<dart::error_t>& e) {
+    std::cerr << e.error();
+  }
+}
