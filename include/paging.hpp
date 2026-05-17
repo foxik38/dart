5305a0278f129a4af033c77e7672883cded19c5b
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-17 22:07:27 +0200

    refactor: rename alignment function and remove const return types
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- include/paging.hpp
+++ include/paging.hpp
@@ -5,15 +5,15 @@
 namespace dart::paging {
   enum class Mode : uint64_t { kStandard = 4 * 1024, kHuge = 2 * 1024 * 1024 };
 
-  constexpr std::pair<const uint64_t, const Mode> AlignSize(uint64_t requested_size) {
+  constexpr std::pair<uint64_t, Mode> Align(uint64_t requested_size) {
     const Mode deducted_mode {
       requested_size >= std::to_underlying(Mode::kHuge) ? Mode::kHuge : Mode::kStandard };
     const uint64_t mode_value = std::to_underlying(deducted_mode);
     const uint64_t aligned_size{ (requested_size + mode_value - 1) & ~(mode_value - 1) };
 
-    return std::pair(
+    return std::pair{
       aligned_size,
       deducted_mode
-    );
+    };
   }
 }
