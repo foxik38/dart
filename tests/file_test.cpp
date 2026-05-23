133a00e18ef6736b3b5c1a981ad1b38fcbb48d03
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-23 22:07:31 +0200

    test: implement array of `File` instances
    `file_test.cpp` now uses a `std::array` where it stores `File` instances on the stack and uses a for loop for testing. This introduces flexibility as more instances can be added into the array and automatically tested.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- tests/file_test.cpp
+++ tests/file_test.cpp
@@ -3,12 +3,15 @@
 #include "file.hpp"
 
 TEST(File, CreatesInstance) {
-  auto users = dart::File::Create<8000>("users.db");
-  auto orders = dart::File::Create<16000>("orders.db");
+  const std::array tables{
+    dart::File::Create<8000>({"users.db"}),
+    dart::File::Create<16000>({"orders.db"})
+  };
 
-  EXPECT_EQ(users.size(), 4096 * 2);
-  EXPECT_EQ(orders.size(), 4096 * 4);
+  for (uint32_t i{0}; i < tables.size(); ++i) {
+    EXPECT_EQ(tables[i].page_mode(), dart::file_utils::Page::kStandard);
+    EXPECT_GE(tables[i].file_descriptor(), 0);
 
-  unlink("users.db");
-  unlink("orders.db");
+    unlink(tables[i].c_path());
+  }
 }
