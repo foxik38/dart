7eed35bf021f4c4e210c3760f11bfbb079995a30
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-23 21:54:17 +0200

    feat: integrate `FileConfig` flags into `Open()` implementation
    `Open()` function implementation now uses `FileConfig` flags, namely `read_only` and `override` booleans. They affect creation or flag status of the file by dynamically building POSIX/Kernel flags with bitwise operations, making the code highly readable.
    
    Resolves local variable shadowing to improve readability.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- src/file.cpp
+++ src/file.cpp
@@ -5,15 +5,30 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#include <iostream>
-
 namespace dart {
 
   std::expected<void, error_t> File::Open() noexcept {
-    if (path().empty() || !path().ends_with(".db") || path().contains("/"))
+    if (config_.path.empty() || !config_.path.ends_with(".db") ||
+        config_.path.contains("/"))
       return std::unexpected(EINVAL);
 
-    int32_t temp_fd = open(c_path(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    int flags = config_.read_only ? O_RDONLY : O_RDWR;
+    int permissions = S_IRUSR;
+
+    flags |= O_CREAT;
+
+    if (config_.read_only && config_.override)
+      return std::unexpected(EINVAL);
+
+    if (!config_.read_only) {
+      permissions |= S_IWUSR;
+
+      if (config_.override)
+        flags |= O_TRUNC;
+    }
+
+    int32_t temp_fd =
+        open(config_.path.c_str(), flags, permissions);
     if (temp_fd == -1) return std::unexpected(errno);
 
     if (struct stat stat_struct{}; fstat(temp_fd, &stat_struct) != -1) {
@@ -25,9 +40,9 @@ namespace dart {
         fstat(temp_fd, &stat_struct);
       }
 
-      const auto [size, page_mode] = paging::Align(stat_struct.st_size);
+      const auto [new_size, page_mode] = file_utils::Align(stat_struct.st_size);
 
-      size_ = size;
+      size_ = new_size;
       page_mode_ = page_mode;
       file_descriptor_ = temp_fd;
 
