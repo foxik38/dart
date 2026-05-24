99c286ca68961bbb6330f302634e810a1e9b49aa
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-24 23:45:45 +0200

    feat: add write performance benchmark
    Updates the main executable to include a 512MB memory-mapped write benchmark. This benchmark creates a 512MB file and writes 8-byte integers in a continuous array until the file is full.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,27 @@
+#include <cstring>
 #include <iostream>
 
 #include "file.hpp"
 
 int main() {
   try {
+    const uint64_t size {dart::file_utils::Megabytes(512)};
+    const uint64_t address_pool {size / 8};
+
     const dart::FileConfig config {
-      .path = "dart.db",
-      .read_only = false,
-      .override = true
+      .path = "benchmark.db",
+      .sync_on = 0
     };
 
-    const dart::File db = dart::File::Create<100'000>(config);
+    dart::File db = dart::File::Create<size>(config);
 
-    std::cout << "path: " << db.path() << '\n';
-    std::cout << "size: " << db.size() << '\n';
-    std::cout << "page_mode: " << std::to_underlying(db.page_mode()) << '\n';
-    std::cout << "pages in total: " << db.size() / std::to_underlying(db.page_mode()) << '\n';
+    uint8_t* address {db.memory_map()};
+
+    for (uint64_t i{0}; i < address_pool; ++i) {
+      const uint64_t offset {i * 8};
+      address[offset] = i;
+    }
   } catch (const std::bad_expected_access<dart::error_t>& e) {
-    std::cerr << e.error();
+    std::cerr << std::strerror(e.error());
   }
 }
