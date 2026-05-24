9c5c3c6a80f9402f4a78f7aa98115488a724b4bc
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-24 22:28:32 +0200

    feat: implement `VerifyConfig()`, `MapMemory()` and `Sync()` member functions
    Source file finally implements `MapMemory()` member function for mapping a file into memory. The function uses the same approach for flags as `Open()` (bitwise operations for merging flags) and uses `madvise(2)` to allow the Linux kernel to use Huge pages for this memory mapping.
    
    This commit also contains implementations of `VerifyConfig()` and `Sync()`, which have been explained in the previous commit.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- src/file.cpp
+++ src/file.cpp
@@ -7,28 +7,27 @@
 
 namespace dart {
 
-  std::expected<void, error_t> File::Open() noexcept {
+  std::expected<void, error_t> File::VerifyConfig() const noexcept {
     if (config_.path.empty() || !config_.path.ends_with(".db") ||
-        config_.path.contains("/"))
+        config_.path.contains("/") || (config_.read_only && config_.override))
       return std::unexpected(EINVAL);
 
+    return {};
+  }
+
+  std::expected<void, error_t> File::Open() noexcept {
     int flags = config_.read_only ? O_RDONLY : O_RDWR;
     int permissions = S_IRUSR;
 
     flags |= O_CREAT;
 
-    if (config_.read_only && config_.override)
-      return std::unexpected(EINVAL);
-
     if (!config_.read_only) {
       permissions |= S_IWUSR;
 
-      if (config_.override)
-        flags |= O_TRUNC;
+      if (config_.override) flags |= O_TRUNC;
     }
 
-    int32_t temp_fd =
-        open(config_.path.c_str(), flags, permissions);
+    int32_t temp_fd = open(config_.path.c_str(), flags, permissions);
     if (temp_fd == -1) return std::unexpected(errno);
 
     if (struct stat stat_struct{}; fstat(temp_fd, &stat_struct) != -1) {
@@ -52,7 +51,37 @@ namespace dart {
     return std::unexpected(errno);
   }
 
-  std::expected<void, error_t> File::MapMemory() noexcept { return {}; }
+  std::expected<void, error_t> File::MapMemory() noexcept {
+    if (!memory_map_) {
+      int mem_protection = PROT_READ;
+
+      if (!config_.read_only) mem_protection |= PROT_WRITE;
+
+      void* temp_mmap =
+          mmap(nullptr, size_, mem_protection, MAP_SHARED, file_descriptor_, 0);
+
+      if (temp_mmap == MAP_FAILED) return std::unexpected<error_t>(errno);
+
+      memory_map_ = static_cast<uint8_t*>(temp_mmap);
+
+      if (page_mode_ == file_utils::Page::kHuge) {
+        if (madvise(memory_map_, size_, MADV_HUGEPAGE) == -1)
+          return std::unexpected(errno);
+      }
+    }
+    return {};
+  }
+
+  std::expected<void, error_t> File::Sync() noexcept {
+    if (config_.sync_on != 0) {
+      if (write_counter_ == config_.sync_on) {
+        if (msync(memory_map_, size_, MS_SYNC) == -1)
+          return std::unexpected(errno);
+
+        write_counter_ = 0;
+      }
+    } return {};
+  }
 
   void File::CloseAndUnmapMemory() noexcept {
     if (file_descriptor_ != -1) {
@@ -65,4 +94,4 @@ namespace dart {
       memory_map_ = nullptr;
     }
   }
-}  // namespace dart
+}
