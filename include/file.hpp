cd0f1161e59c3f0cd51940020d090b793b472177
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-27 20:41:14 +0200

    refactor: adapt `File` class for new project structure
    The `File` class is now acting as a custom POSIX system call wrapper that does not hold its own configuration struct or performs its own constraint validations.
    
    Due to a major shrinkage of the class, the alignment has been lowered to 32 bytes (1/2 of a cache line on AMD64).
    
    As the `File` class is no longer acting as the frontend of the manageable code, all public getters have been removed as well as removed temporarily some functions until a new solution is implemented for syncing.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- include/file.hpp
+++ include/file.hpp
@@ -1,87 +1,33 @@
 #pragma once
 #include <expected>
-#include <limits>
-#include <string>
-#include <string_view>
-#include <utility>
 
+#include "config.hpp"
 #include "file_utils.hpp"
 
 namespace dart {
   using error_t = int32_t;
 
-  struct FileConfig {
-    std::string path;
-    std::string directory{"/"};
-    uint32_t sync_on{0};
-    bool read_only{false};
-    bool override{false};
-  };
-
-  template <uint64_t T>
-  concept IsValidSize =
-      T >= 4096 &&
-      T < static_cast<uint64_t>(std::numeric_limits<uint32_t>::max()) * 4 - 4;
-
-  class alignas(64) File {
+  class alignas(32) File {
    public:
-    template <uint64_t T>
-      requires IsValidSize<T>
-    static File Create(FileConfig config) {
-      return File(std::move(config), T);
-    }
-
-    void RegisterWrite() {
-      HandleResult(Sync());
-      ++write_counter_;
-    }
-
-    [[nodiscard]] uint64_t size() const noexcept { return size_; }
-
-    [[nodiscard]] std::string_view path() const noexcept {
-      return config_.path;
-    }
-
-    [[nodiscard]] const char* c_path() const noexcept {
-      return config_.path.c_str();
-    }
-
-    [[nodiscard]] file_utils::Page page_mode() const noexcept {
-      return page_mode_;
-    }
-
-    [[nodiscard]] uint8_t* memory_map() const noexcept { return memory_map_; }
-
-    [[nodiscard]] int32_t file_descriptor() const noexcept {
-      return file_descriptor_;
+    explicit File(Config& config) : config_{config} {
+      HandleResult(Open().and_then([&] { return MapMemory(); }));
     }
 
     File& operator=(const File&) = delete;
     File& operator=(File&&) = delete;
     File(const File&) = delete;
-    File(File&&) = delete;
+    File(File&& old) = delete;
 
     ~File() { CloseAndUnmapMemory(); }
 
    private:
-    FileConfig config_;
-    uint64_t size_;
+    Config& config_;
     file_utils::Page page_mode_{file_utils::Page::kStandard};
     uint8_t* memory_map_{nullptr};
     int32_t file_descriptor_{-1};
-    uint32_t write_counter_{0};
 
-    explicit File(FileConfig&& config, uint64_t size)
-        : config_{std::move(config)}, size_{size} {
-      HandleResult(VerifyConfig()
-        .and_then([&] { return Open(); })
-        .and_then([&] { return MapMemory(); }));
-    }
-
-    [[nodiscard]] std::expected<void, error_t> VerifyConfig() const noexcept;
     [[nodiscard]] std::expected<void, error_t> Open() noexcept;
     [[nodiscard]] std::expected<void, error_t> MapMemory() noexcept;
-    [[nodiscard]] std::expected<void, error_t> Sync() noexcept;
     void CloseAndUnmapMemory() noexcept;
 
     static void HandleResult(std::expected<void, error_t> result) {
