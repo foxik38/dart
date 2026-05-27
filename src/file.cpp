91b3319b7d068b7d4cef8a177ec7330a047dc04a
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-27 21:10:06 +0200

    fix: adapt `File` source file and fix logic bugs
    The `File` source file now uses the `Config` reference private member to read and update all the necessary configuration struct members.
    
    Fixes a bug when even in the read-only mode, the code could still reach statements that were changing the state of the file (such as truncation). Other bugs are related to `CloseAndUnmapMemory()` which now implements the correct code execution flow and forces a sync with the file prior unmapping mapped memory.
    
    Lastly, a result check of the second `fstat(2)` has been implemented to make the code more robust.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- src/file.cpp
+++ src/file.cpp
@@ -4,44 +4,32 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <cerrno>
 
 namespace dart {
-
-  std::expected<void, error_t> File::VerifyConfig() const noexcept {
-    if (config_.path.empty() || !config_.path.ends_with(".db") ||
-        config_.path.contains("/") || (config_.read_only && config_.override))
-      return std::unexpected(EINVAL);
-
-    return {};
-  }
-
   std::expected<void, error_t> File::Open() noexcept {
-    int flags = config_.read_only ? O_RDONLY : O_RDWR;
+    config_.size = file_utils::Align(config_.size).first;
+
+    int flags = config_.read_only ? O_RDONLY : (O_RDWR | O_CREAT);
     int permissions = S_IRUSR;
 
-    flags |= O_CREAT;
+    if (!config_.read_only) permissions |= S_IWUSR;
 
-    if (!config_.read_only) {
-      permissions |= S_IWUSR;
-
-      if (config_.override) flags |= O_TRUNC;
-    }
-
-    int32_t temp_fd = open(config_.path.c_str(), flags, permissions);
+    int32_t temp_fd = open(config_.name, flags, permissions);
     if (temp_fd == -1) return std::unexpected(errno);
 
     if (struct stat stat_struct{}; fstat(temp_fd, &stat_struct) != -1) {
-      if (stat_struct.st_size == 0) {
-        if (ftruncate(temp_fd, static_cast<__off_t>(size_)) == -1) {
+      if (stat_struct.st_size == 0 && !config_.read_only) {
+        if (ftruncate(temp_fd, static_cast<off_t>(config_.size)) == -1) {
           close(temp_fd);
           return std::unexpected(errno);
         }
-        fstat(temp_fd, &stat_struct);
+        if (fstat(temp_fd, &stat_struct) == -1) return std::unexpected(errno);
       }
 
       const auto [new_size, page_mode] = file_utils::Align(stat_struct.st_size);
 
-      size_ = new_size;
+      config_.size = new_size;
       page_mode_ = page_mode;
       file_descriptor_ = temp_fd;
 
@@ -57,41 +45,31 @@ namespace dart {
 
       if (!config_.read_only) mem_protection |= PROT_WRITE;
 
-      void* temp_mmap =
-          mmap(nullptr, size_, mem_protection, MAP_SHARED, file_descriptor_, 0);
+      void* temp_mmap = mmap(nullptr, config_.size, mem_protection, MAP_SHARED,
+                             file_descriptor_, 0);
 
       if (temp_mmap == MAP_FAILED) return std::unexpected<error_t>(errno);
 
       memory_map_ = static_cast<uint8_t*>(temp_mmap);
 
       if (page_mode_ == file_utils::Page::kHuge) {
-        if (madvise(memory_map_, size_, MADV_HUGEPAGE) == -1)
+        if (madvise(memory_map_, config_.size, MADV_HUGEPAGE) == -1)
           return std::unexpected(errno);
       }
     }
     return {};
   }
 
-  std::expected<void, error_t> File::Sync() noexcept {
-    if (config_.sync_on != 0) {
-      if (write_counter_ == config_.sync_on) {
-        if (msync(memory_map_, size_, MS_SYNC) == -1)
-          return std::unexpected(errno);
-
-        write_counter_ = 0;
-      }
-    } return {};
-  }
-
   void File::CloseAndUnmapMemory() noexcept {
+    if (memory_map_) {
+      msync(memory_map_, config_.size, MS_SYNC);
+      munmap(memory_map_, config_.size);
+      memory_map_ = nullptr;
+    }
+
     if (file_descriptor_ != -1) {
       close(file_descriptor_);
       file_descriptor_ = -1;
-    }
-
-    if (memory_map_) {
-      munmap(memory_map_, size_);
-      memory_map_ = nullptr;
     }
   }
 }
