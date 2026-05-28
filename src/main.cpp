92a277f7df496374084d1c61589a15cc41213a06
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-29 00:04:12 +0200

    refactor: update `main.cpp` to reflect `table.hpp` changes
    Main executable now tests the newly implemented move semantics of `Table` class and verifies the typename constraint checks.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,20 @@
 #include "table.hpp"
 
 int main() {
-  constexpr dart::Config cfg {"users.db", 4096};
-  auto copy_cfg = cfg;
+  constexpr dart::Config cfg {"orders.db", 4096 * 2};
 
-  std::array tables {
-    dart::Table::Construct<cfg>(),
-    dart::Table::Construct<{"orders.db", 8192}>()
+  struct Users {
+    char pwd[128];
+    char user[32];
+    uint64_t id;
   };
 
-  for (auto& table: tables) {
-    std::cout << table.size() << '\n';
-  }
+  auto users = dart::Table<Users, {"users.db", 4096}>();
+
+  std::cout << "users: " << users.size() << '\n'; // 4096 (size)
+
+  auto users_new = std::move(users);
+
+  std::cout << "users: " << users.size() << '\n'; // 0 (MOVED)
+  std::cout << "users_new: " << users_new.size() << '\n'; // 4096 (MOVED)
 }
