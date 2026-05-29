b02dd707925fe89a167aa63ad4dbe266212b0735
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-29 23:10:40 +0200

    feat: integrate `TableData` and `Flags` structs and update constraint checks
    The `Table` class now saves new structs in two separate members that are accessible through new getters `data()` and `flags() const`.
    
    Updates move constructor to reflect a move logic for the `flags_` member.
    
    Constraint concept `IsValidStruct` now disables raw pointer by using a type trait `std::is_pointer_v<T>` to prevent user error.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- include/table.hpp
+++ include/table.hpp
@@ -1,38 +1,40 @@
 #pragma once
 
-#include "config.hpp"
+#include "defs.hpp"
 
 namespace dart {
   template <typename T>
-  concept IsValidStruct = std::is_trivially_copyable_v<T> && std::is_class_v<T>;
+  concept IsValidStruct = std::is_trivially_copyable_v<T> &&
+                          std::is_class_v<T> && !std::is_pointer_v<T>;
 
-  template <Config C>
-  concept IsValidConfig = C.size >= 4096 && C.size <= (1ULL << 32) &&
-                          std::string_view(C.name).contains(".db");
+  template <TableData D>
+  concept IsValidData = D.size >= 4096 && D.size <= (1ULL << 32) &&
+                        std::string_view(D.name).contains(".db");
 
-  template <typename T, Config C>
-    requires IsValidStruct<T> && IsValidConfig<C>
+  template <typename T, TableData D, Flags F>
+    requires IsValidStruct<T> && IsValidData<D>
   class alignas(64) Table {
    public:
     using table_t = T;
 
     constexpr explicit Table() {}
 
-    [[nodiscard]] constexpr std::string_view name() const noexcept {
-      return config_.name;
-    }
+    [[nodiscard]] constexpr TableData& data() noexcept { return table_data_; }
 
-    [[nodiscard]] constexpr uint64_t size() const noexcept {
-      return config_.size;
-    }
+    [[nodiscard]] constexpr Flags flags() const noexcept { return flags_; }
 
     Table& operator=(const Table&) = delete;
     Table& operator=(Table&&) = delete;
     Table(const Table&) = delete;
 
-    Table(Table&& old) : config_{std::move(old.config_)} { old.config_ = {}; }
+    Table(Table&& old)
+        : flags_{std::move(old.flags_)},
+          table_data_{std::move(old.table_data_)} {
+      old.table_data_ = {}, old.flags_ = {};
+    }
 
    private:
-    Config config_{C};
+    Flags flags_{F};
+    TableData table_data_{D};
   };
 }
