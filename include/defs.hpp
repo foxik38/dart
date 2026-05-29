ef2b0cf195a33b6f7eb964c663ed24b299453006
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-29 22:55:57 +0200

    refactor: split `Config` struct onto `TableData` and `Flags`
    Old `Config` struct has been replaced with two structs: `TableData` for storing mutable data about the `Table` class and `Flags` for storing constant flags for any class.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- include/defs.hpp
+++ include/defs.hpp
@@ -2,9 +2,12 @@
 #include <cstdint>
 
 namespace dart {
-  struct Config {
+  struct TableData {
     char name[32];
     uint64_t size;
+  };
+
+  struct Flags {
     uint32_t sync_on{0};
     bool read_only{false};
   };
