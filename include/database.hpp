f850366bacc2166d7b34149a24666df8c5b30e97
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-30 22:37:19 +0200

    feat: create `Database` class skeleton
    The `Database` class performs struct identity and parameter pack size constraint checks (`IsTable` and `IsValidTableCount`) to make sure the `Database` class receives `Table` classes in an allowed count.
    
    Class constructor takes r-value reference parameter pack, as moving the `Table`... pack is the only allowed operation (as the `Database` class is now the owner of `Table` classes). Copying is disabled on purpose.
    
    Deletes move/copy assignment operators and move/copy constructors of the `Database` class to ensure uniqueness of the class and prevent issues brought by moving the class.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- include/database.hpp
+++ include/database.hpp
@@ -1,5 +1,38 @@
 #pragma once
+#include <tuple>
+
+#include "table.hpp"
 
 namespace dart {
-  class Database {};
+  template <typename T>
+  concept IsTable = requires(T t) {
+    typename T::table_t;
+    { t.data() } -> std::same_as<TableData&>;
+    { t.flags() } -> std::same_as<Flags>;
+  };
+
+  template <typename... T>
+  concept IsValidTableCount = sizeof...(T) >= 1 &&
+    sizeof...(T) <= std::numeric_limits<uint8_t>::max();
+
+  template <typename... T>
+    requires IsValidTableCount<T...> && (IsTable<T> && ...)
+  class Database {
+   public:
+    constexpr explicit Database(T&&... tables)
+        : tables_{std::move(tables)...} {}
+
+    template <uint64_t Tt>
+    [[nodiscard]] constexpr auto& table() noexcept {
+      return std::get<Tt>(tables_);
+    }
+
+    Database& operator=(const Database&) = delete;
+    Database& operator=(Database&&) = delete;
+    Database(const Database&) = delete;
+    Database(Database&&) = delete;
+
+   private:
+    std::tuple<T...> tables_;
+  };
 }
