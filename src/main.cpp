42b8d4fc077a63d164e5d94b9bdb0dc65d1966ae
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-30 23:24:02 +0200

    feat: implement code example in `main.cpp`
    Main executable runs a simple code example which creates `Users` and `Products` structs, creates two `TableData` constexpr variables used in two `Table` instances to build the tables. Once they are built, they are moved to the `Database` instance in two possible ways shown in the code.
    
    Described code execution:
    
    1. Definition of `Users` and `Products` structs.
    2. Definition of `TableData` instances later passed into `Table` instances.
    3. Initialization of `Table` instances assigned to `users` and `products`.
    4. Initialization of `Database` instance and move of `users` and `products` `Table` instances into the `Database` instance (ownership change).
    5. Initialization of `Database` instance using initialization of `Table` instances as arguments (no ownership change, `Database` becomes first owner of those r-values).
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 
+#include "defs.hpp"
 #include "database.hpp"
-#include "file.hpp"
 #include "table.hpp"
 
 int main() {
-  constexpr dart::Config cfg {"orders.db", 4096 * 2};
-
   struct Users {
     char pwd[128];
     char user[32];
     uint64_t id;
   };
 
-  auto users = dart::Table<Users, {"users.db", 4096}>();
+  struct Products {
+    char id[16];
+    char title[32];
+    uint64_t price;
+  };
 
-  std::cout << "users: " << users.size() << '\n'; // 4096 (size)
+  constexpr dart::TableData users_data {"users.db", 4096};
+  constexpr dart::TableData products_data {"products.db", 4096 * 2};
 
-  auto users_new = std::move(users);
+  auto users = dart::Table<Users, users_data>();
+  auto products = dart::Table<Products, products_data>();
 
-  std::cout << "users: " << users.size() << '\n'; // 0 (MOVED)
-  std::cout << "users_new: " << users_new.size() << '\n'; // 4096 (MOVED)
+  dart::Database(std::move(users), std::move(products));
+
+  //OR
+
+  dart::Database(
+    dart::Table<Users, users_data>(),
+    dart::Table<Products, products_data>()
+  );
 }
