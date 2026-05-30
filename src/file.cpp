dba29ebf3066e8bbec47fbdf0b1d5b743e62ff4e
foxik38 <281372635+foxik38@users.noreply.github.com>
2026-05-30 23:07:42 +0200

    refactor: improve robustness and adapt code to use `TableData` and `Flags` structs
    `Open()` implementation now closes temporary file descriptor if the second `fstat(2)` fails and `MapMemory()` implementation sets `memory_map_` to `nullptr` if `madvise(2)` fails leading to higher robustness of the code.
    
    Changes `config_` to either `data_` or `flags_`, depending on the usage to adapt code to recent changes.
    
    Signed-off-by: foxik38 <281372635+foxik38@users.noreply.github.com>

--- src/file.cpp
+++ src/file.cpp
@@ -8,28 +8,31 @@
 
 namespace dart {
   std::expected<void, error_t> File::Open() noexcept {
-    config_.size = file_utils::Align(config_.size).first;
+    data_.size = file_utils::Align(data_.size).first;
 
-    int flags = config_.read_only ? O_RDONLY : (O_RDWR | O_CREAT);
+    int flags = flags_.read_only ? O_RDONLY : (O_RDWR | O_CREAT);
     int permissions = S_IRUSR;
 
-    if (!config_.read_only) permissions |= S_IWUSR;
+    if (!flags_.read_only) permissions |= S_IWUSR;
 
-    int32_t temp_fd = open(config_.name, flags, permissions);
+    int32_t temp_fd = open(data_.name, flags, permissions);
     if (temp_fd == -1) return std::unexpected(errno);
 
     if (struct stat stat_struct{}; fstat(temp_fd, &stat_struct) != -1) {
-      if (stat_struct.st_size == 0 && !config_.read_only) {
-        if (ftruncate(temp_fd, static_cast<off_t>(config_.size)) == -1) {
+      if (stat_struct.st_size == 0 && !flags_.read_only) {
+        if (ftruncate(temp_fd, static_cast<off_t>(data_.size)) == -1) {
           close(temp_fd);
           return std::unexpected(errno);
         }
-        if (fstat(temp_fd, &stat_struct) == -1) return std::unexpected(errno);
+        if (fstat(temp_fd, &stat_struct) == -1) {
+          close(temp_fd);
+          return std::unexpected(errno);
+        }
       }
 
       const auto [new_size, page_mode] = file_utils::Align(stat_struct.st_size);
 
-      config_.size = new_size;
+      data_.size = new_size;
       page_mode_ = page_mode;
       file_descriptor_ = temp_fd;
 
@@ -43,9 +46,9 @@ namespace dart {
     if (!memory_map_) {
       int mem_protection = PROT_READ;
 
-      if (!config_.read_only) mem_protection |= PROT_WRITE;
+      if (!flags_.read_only) mem_protection |= PROT_WRITE;
 
-      void* temp_mmap = mmap(nullptr, config_.size, mem_protection, MAP_SHARED,
+      void* temp_mmap = mmap(nullptr, data_.size, mem_protection, MAP_SHARED,
                              file_descriptor_, 0);
 
       if (temp_mmap == MAP_FAILED) return std::unexpected<error_t>(errno);
@@ -53,7 +56,8 @@ namespace dart {
       memory_map_ = static_cast<uint8_t*>(temp_mmap);
 
       if (page_mode_ == file_utils::Page::kHuge) {
-        if (madvise(memory_map_, config_.size, MADV_HUGEPAGE) == -1)
+        if (madvise(memory_map_, data_.size, MADV_HUGEPAGE) == -1)
+          memory_map_ = nullptr;
           return std::unexpected(errno);
       }
     }
@@ -62,8 +66,8 @@ namespace dart {
 
   void File::CloseAndUnmapMemory() noexcept {
     if (memory_map_) {
-      msync(memory_map_, config_.size, MS_SYNC);
-      munmap(memory_map_, config_.size);
+      msync(memory_map_, data_.size, MS_SYNC);
+      munmap(memory_map_, data_.size);
       memory_map_ = nullptr;
     }
 
